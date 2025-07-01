9016043e5a9050b8cc736e16c3124eafa5edf886
Skmdzuneth <zuneth111@gmail.com>
2025-07-01 13:14:44 +0530

    Added new Package

---
+++ SampleMaven_01/src/test/java/Zuneth/Launch.java
@@ -0,0 +1,14 @@
+package Zuneth;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Launch {
+	
+	@Test
+	public void demo() {
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
